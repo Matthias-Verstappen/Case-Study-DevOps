fe3ff24d308fd2e3a6531fd25c90e8c8e8792738
Matthias-Verstappen <matthias.verstappen@student.thomasmore.be>
2022-12-18 19:29:31 +0100

    changes

--- Case_study_r0851784/Context.cs
+++ Case_study_r0851784/Context.cs
@@ -14,18 +14,13 @@ namespace Case_study_r0851784
 
 		public IDbConnection GetConnection()
 		{
-			return new SqliteConnection(@"Data Source=C:\Devops\BerkvenDB.db;");
+			return new SqliteConnection(@"Data Source=.\BerkvenDB.db;New=true");
 		}
 
 		public void createDatabase()
 		{
 			using (IDbConnection db = GetConnection())
 			{
-				//db.Execute("DROP TABLE PERSONS");
-				//db.Execute("DROP TABLE LEADERS");
-				//db.Execute("CREATE TABLE IF NOT EXISTS Roles(ID int PRIMARY KEY, Name varchar(50))");
-				//db.Execute("CREATE TABLE IF NOT EXISTS Persons(ID int AUTO_INCREMENT PRIMARY KEY, Name varchar(50), Email varchar(50), Drinks int, Role varchar(50))");
-				//db.Execute("CREATE TABLE IF NOT EXISTS Leaders (ID int AUTO_INCREMENT PRIMARY KEY, Tasks varchar(50)) INHERITS (Persons);");
 				db.Execute("CREATE TABLE IF NOT EXISTS Persons(ID INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT, Email TEXT, Drinks INTEGER, PlayGroup TEXT)");
 				db.Execute("CREATE TABLE IF NOT EXISTS Leaders(ID INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT, Email TEXT, Drinks INTEGER, PlayGroup TEXT, Tasks TEXT)");
 
